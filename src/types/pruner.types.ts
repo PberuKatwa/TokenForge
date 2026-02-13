f43139d91001e2a8483a673f406f6fdfe5045a89
Pberu <pberu8531@gmail.com>
2026-02-13 09:59:20 +0300

    pruner types setup v1:setup file

---
+++ src/types/pruner.types.ts
@@ -0,0 +1,41 @@
+export type Speaker = "Fellow" | string;
+
+export interface RawTurn {
+  speaker: Speaker;
+  text: string;
+}
+
+export interface Session {
+  session_topic: string;
+  duration_minutes: number;
+  transcript: RawTurn[];
+}
+
+export interface ProcessedTurn extends RawTurn {
+  _compressed?: boolean;
+}
+
+export interface EvalMetadata {
+  session_topic: string;
+  original_turns: number;
+  original_word_count: number;
+  extracted_signals: {
+    safety: number;
+    pedagogy: number;
+    facilitation: number;
+  };
+  participation_score: number;
+  final_payload_turns: number;
+  final_word_count: number;
+  word_reduction_ratio: string;
+}
+
+export interface EvalPayload {
+  metadata: EvalMetadata;
+  evaluation_ready_transcript: ProcessedTurn[];
+}
+
+interface ScoredTurn extends RawTurn {
+  index: number;
+  score: number;
+}
