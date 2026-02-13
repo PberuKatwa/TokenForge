75b99992f158d0411a9a9f37d47d12a4ce72d3ab
Pberu <pberu8531@gmail.com>
2026-02-13 14:46:24 +0300

    transcript pruner implementation v4:created token service

---
+++ src/tokenService/token.service.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+export async function tokenService() {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+}
