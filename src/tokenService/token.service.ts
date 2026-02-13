5ff21bb8e1e8d5e7a2bbe49b8ee3922b21dd4190
Pberu <pberu8531@gmail.com>
2026-02-13 14:52:47 +0300

    transcript pruner implementation v4:created token service

--- src/tokenService/token.service.ts
+++ src/tokenService/token.service.ts
@@ -1,9 +1,25 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
+import { Session } from '../types/pruner.types.js';
+import { initializePruner } from '../transcriptPruner/index.pruner.js';
+import { safetyWords, pedagogyWords, reflectionWords, empathyWords, understandingWords,fillerWords } from '../tokenOptimizer/config.pruner.js';
+
 
 export async function tokenService() {
   try {
+
+    // const inputFileName = "sessionFail.json";
+    const inputFileName = "sessionAverage.json";
+    // const inputFileName = "sessionPerfect.json";
+    const filePath = path.join(__dirname, "sessionJson", inputFileName);
+
+    console.log(`Reading file from: ${filePath}`);
+
+    const data = await fs.readFile(filePath, "utf-8");
+    const sessionData: Session = JSON.parse(data);
+
+    const answer = await initializePruner(0, 100, 20, true, safetyWords,pedagogyWords,reflectionWords,empathyWords,understandingWords,fillerWords,sessionData)
 
   } catch (error) {
     throw error;
