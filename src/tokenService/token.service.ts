46fe883a5f4586054ec6fc919b741fe90596fb5a
Pberu <pberu8531@gmail.com>
2026-02-16 18:28:12 +0300

    token service v1:working service

--- src/tokenService/token.service.ts
+++ src/tokenService/token.service.ts
@@ -1,12 +1,7 @@
 import { Lexicons, PrunedSession, Session } from '../types/pruner.types.js';
 import { initializePruner } from '../transcriptPruner/index.pruner.js';
-import { allLexicons } from './token.config.js';
-import { SessionSchema } from '../validators/session.schema.js';
-import { evaluateWithGemini } from '../gemini/gemini.js';
-import { evaluateFullTranscript } from '../gemini/gemini.full.js';
 
-
-export async function tokenService(lexicons: Lexicons, session: Session):PrunedSession{
+export async function tokenService(lexicons: Lexicons, session: Session):Promise<PrunedSession>{
   try {
 
     const pruner = initializePruner(0, 20);
